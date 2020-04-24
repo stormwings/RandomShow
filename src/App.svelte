6241a963a31354c690e6791b66d805cb6cb81e0a
Mariano V <marianov994@gmail.com>
2020-04-24 01:56:54 -0300

    layout and first card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,38 @@
 <script>
+	import Card from './components/Card.svelte'
 	export let name;
+	export let lastName;
+	
+	setTimeout(() => lastName = 'holus', 3000)
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!-- <h1>Hello {name} {lastName}!</h1> -->
+	<h1>RandomShow!</h1>
+	<p>Estos son los últimos juegos que puedes descargar</p>
+	<p>Powered by <a href="https://svelte.dev/tutorial">CrackWatch</a>.</p>
+
+	<Card />
 </main>
 
 <style>
+	/* variable globals */
+	:global(:root) {
+		--background-color: #292C4F;
+		--background-color__card: #363868;
+
+		--text-primary: #292C4F;
+		--text-default: #FEFEFE;
+		--text-info: #59D2E0;
+		--text-danger: #ff3e00;
+	}
+
+	/* global styles */
+	:global(body) {
+		background-color: var(--background-color);
+		color: var(--text-default);
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,10 +41,10 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: var(--text-danger);
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		font-size: 2em;
+		font-weight: 200;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/Card.svelte
@@ -0,0 +1,84 @@
+<script>
+  const goToBuy = () => console.log('Go buy!')
+  const goToDownload = () => console.log('Go pirate!')
+  const imageProfile = 'https://i1.wp.com/culturageek.com.ar/wp-content/uploads/2020/03/Culturageek.com_.ar-Resident-Evil-3-Review-1.jpg'
+
+</script>
+
+<main>
+  <div class="card">
+    <div class="card--header">
+      <div class="card--header__title">
+        Resident Evil 3 Remake
+      </div>
+    </div>
+    <div class="card--body">
+      <div class="card--body__image">
+        <img src={imageProfile} alt="Resident evil 3 banner">
+      </div>
+      <div class="card--body__description">
+        El Juego aún no ha sido crackeado 💔
+      </div>
+    </div>
+    <div class="card--footer">
+      <button class="card--footer__button" on:click={goToBuy}>
+        Ir a comprar
+      </button>
+      <button class="card--footer__button" on:click={goToDownload}>
+        Descargar Pirata
+      </button>
+    </div>
+  </div>
+</main>
+
+<style>
+  .card {
+    width: 100%;
+    max-width: 600px;
+    border-radius: 15px;
+    background-color: var(--background-color__card);
+  }
+
+  .card--header {
+    padding-top: 15px;
+    padding-bottom: 10px;
+    min-height: 20px;
+  }
+
+  .card--header__title {
+    font-size: 1.2em;
+  }
+
+  .card--body__image {
+    border-radius: 15px;
+    overflow: hidden;
+    margin-bottom: 5px;
+  }
+
+  .card--body__image img {
+    width: 100%;
+  }
+
+  .card--body {
+    padding: 5px 10px;
+    min-height: 20px;
+  }
+
+  .card--body__description {
+    font-size: 0.9em;
+  }
+
+  .card--footer {
+    padding: 15px 0;
+  }
+  .card--footer__button {
+    border: none;
+    border-radius: 5px;
+    background-color: var(--text-info);
+    color: var(--text-primary);
+    font-size: 15px;
+    margin: 0px 2px;
+  }
+
+
+</style>
